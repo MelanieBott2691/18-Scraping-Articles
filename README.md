71cf92fb474cd6217f9c67b859bba1ff33db3ff8
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-07-10 00:18:22 -0600

    redid file set up to include handlebars

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # 18-Scrapping
+
+Heroku link https://scrapping-articles.herokuapp.com/
+GitHub link https://melaniebott2691.github.io/18-Scrapping/
+
+Web app that lets users view and leave comments on news articles using Mongoose and Cheerio to scrape news from the news site.
+
+## Instructions for application
+
+#### To run application run npm init. 
+##### Installation of npm packages:
+- express
+- express-handlebars
+- mongoose
+- cheerio
+- axios
+- body-parser
+
+Deployment of application to Heroku set with mLab provision.
+
+#### When users visit the site the app will scrape news articles from https://kotaku.com/ and display them for the user. Each article that is scraped will save to the applications database displaying: 
+- Title
+- Summary
+- URL
--- models/Comment.js
+++ models/Comment.js
@@ -5,30 +5,20 @@ var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var ArticleSchema = new Schema({
+var CommentSchema = new Schema({
   // `title` is required and of type String
   title: {
-    type: String,
-    required: true
+    type: String
   },
   // `link` is required and of type String
-  link: {
+  body: {
     type: String,
     required: true
-  },
-
-  // The ref property links the ObjectId to the Comment model
-  // This allows us to populate the Article with an associated Comment
-  comment: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Comment'
-    }
-  ]
+  }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model('Article', ArticleSchema);
+var Comment = mongoose.model('Comment', CommentSchema);
 
 // Export the Article model
-module.exports = Article;
+module.exports = Comment;
