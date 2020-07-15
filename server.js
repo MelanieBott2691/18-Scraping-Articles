9dcc3fefbb6a6e8f83e828a603f4b82e161ac51e
MelanieBott2691 <melaniebott@gmail.com>
2020-07-14 22:13:18 -0600

    still not scraping in heroku, added images of progress to README

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ mongoose.connect('mongodb://localhost/unit18Populater', {
   useNewUrlParser: true
 })
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI)
+
 // Routes
 app.get('/scrape', function (req, res) {
   axios.get('http://www.kotaku.com/').then(function (response) {
