6368e817a8514167e006864927644d15b5157240
MelanieBott2691 <melaniebott@gmail.com>
2020-07-21 23:58:37 -0600

    scraping button scrapes but for some reason the articles are not showing, just undefined but everything else seems to be working

--- public/app.js
+++ public/app.js
@@ -52,3 +52,16 @@ $(document).on('click', '#savenote', function () {
   $('#titleinput').val('')
   $('#bodyinput').val('')
 })
+$('#scrape-btn').on('click', function (event) {
+  console.log('scraping articles...')
+  event.preventDefault()
+
+  $('#article-body').empty()
+
+  $.ajax('/scrape', {
+    type: 'GET'
+  }).then(function () {
+    console.log('Scrape complete')
+    window.location = '/'
+  })
+})
