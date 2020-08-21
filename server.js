883db39d1aff33be27dc9d918125e4cd15e7616e
MelanieBott2691 <melaniebott@gmail.com>
2020-08-20 21:37:46 -0600

    depreciation error: fixed with mongoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true }) .then(()=> console.log('Database Connection Successful!!')) .catch(err => console.error(err));

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
 var express = require('express');
 var exphbs = require('express-handlebars');
-var logger = require('morgan');
+
 var mongoose = require('mongoose');
 
 var PORT = process.env.PORT || 3000;
 // initialize express
 var app = express();
+// get routes
+var routes = require('./routes');
 
-app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
@@ -19,15 +19,15 @@ var MONGODB_URI =
 
 // Connect to the Mongo DB
 // mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database Connection Successful!!'))
+  .catch((err) => console.error(err));
+mongoose.set('useCreateIndex', true);
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
-// Routes
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
+app.use(routes);
 // Start the server
 app.listen(PORT, function () {
   console.log('Listening on port ' + PORT + '!');
