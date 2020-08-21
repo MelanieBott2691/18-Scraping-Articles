5bafec100056de282781e3bd4464d0718f9c8448
MelanieBott2691 <melaniebott@gmail.com>
2020-08-20 19:04:55 -0600

    pulled from master but now erroring, working with note and article models

--- models/Note.js
+++ models/Note.js
@@ -5,30 +5,30 @@ var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new NoteSchema object
 // This is similar to a Sequelize model
-var noteSchema = new Schema({
-    _articleId: {
-        type: Schema.Types.ObjectId,
-        ref: "Article"
-    },
+var NoteSchema = new Schema({
+  _articleId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Article'
+  },
+  title: {
+    type: String
+  },
 
-
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    noteText: String
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  noteText: String
 });
 // articleId: {
 //   type: String
-
 
 // title: String,
 // // `body` is of type String
 // body: String
 
-
 // This creates our model from the above schema, using mongoose's model method
-var Note = mongoose.model('Note', noteSchema);
+var Note = mongoose.model('Note', NoteSchema);
 
 // Export the Note model
 module.exports = Note;
--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var express = require('express');
-var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser");
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+// Require Note and Article models
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js');
 // var axios = require('axios');
-var request = require("request");
+var request = require('request');
 var cheerio = require('cheerio');
 
 // Require all models
-var db = require("./models");
+var db = require('./models');
 
 var PORT = process.env.PORT || 3000;
 // initialize express
 var app = express();
-var routes = require("./routes")
+var routes = require('./routes');
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,13 +25,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.json());
 app.use(express.static('public'));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // var Article = require("./models/Article.js");
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI =
-    process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 
 // Connect to the Mongo DB
 // mongoose.Promise = Promise;
@@ -169,6 +172,6 @@ mongoose.connect(MONGODB_URI);
 // });
 
 // Start the server
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+app.listen(PORT, function () {
+  console.log('App running on port ' + PORT + '!');
 });
