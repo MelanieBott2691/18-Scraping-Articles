e4414da5769a8104fe60aa873419fb8b10c78e1d
MelanieBott2691 <melaniebott@gmail.com>
2020-08-20 20:08:30 -0600

    removed bodyparser

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
+require('dotenv').config();
 var express = require('express');
 var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-
-var axios = require('axios');
-// var request = require('request');
-var cheerio = require('cheerio');
-
-// Require all models
-var db = require('./models');
 
 var PORT = process.env.PORT || 3000;
 // initialize express
 var app = express();
-// var routes = require('./routes');
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-// Connect to the Mongo DB
-mongoose.connect('mongodb://localhost/unit18Populater', {
-  useNewUrlParser: true
-});
-// var Article = require("./models/Article.js");
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
@@ -37,71 +20,85 @@ var MONGODB_URI =
 // Connect to the Mongo DB
 // mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-// var results = [];
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Routes
-app.get('/scrape', function (req, res) {
-  axios.get('http://www.kotaku.com/').then(function (response) {
-    var $ = cheerio.load(response.data);
-    $('article h2').each(function (i, element) {
-      var result = {};
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children('a').text();
-      result.link = $(this).children('a').attr('href');
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function (dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    });
-    res.send('Scrape Complete');
-  });
+// Connect to the Mongo DB
+mongoose.connect('mongodb://localhost/unit18Populater', {
+  useNewUrlParser: true
 });
+// var Article = require("./models/Article.js");
 
-// Route for getting all Articles from the db
-app.get('/articles', function (req, res) {
-  db.Article.find({})
-    .then(function (dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
-});
+// // var results = [];
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get('/articles/:id', function (req, res) {
-  db.Article.findOne({ _id: req.params.id })
-    .populate('note')
-    .then(function (dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
-});
+// // Routes
+// app.get('/scrape', function (req, res) {
+//   axios.get('http://www.kotaku.com/').then(function (response) {
+//     var $ = cheerio.load(response.data);
+//     $('article h2').each(function (i, element) {
+//       var result = {};
 
-app.post('/articles/:id', function (req, res) {
-  db.Note.create(req.body)
-    .then(function (dbNote) {
-      return db.Article.findOneAndUpdate(
-        { _id: req.params.id },
-        { note: dbNote._id },
-        { new: true }
-      );
-    })
-    .then(function (dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
-});
+//       // Add the text and href of every link, and save them as properties of the result object
+//       result.title = $(this).children('a').text();
+//       result.link = $(this).children('a').attr('href');
+
+//       // Create a new Article using the `result` object built from scraping
+//       db.Article.create(result)
+//         .then(function (dbArticle) {
+//           console.log(dbArticle);
+//         })
+//         .catch(function (err) {
+//           console.log(err);
+//         });
+//     });
+//     res.send('Scrape Complete');
+//   });
+// });
+
+// // Route for getting all Articles from the db
+// app.get('/articles', function (req, res) {
+//   db.Article.find({})
+//     .then(function (dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function (err) {
+//       res.json(err);
+//     });
+// });
+
+// // Route for grabbing a specific Article by id, populate it with it's note
+// app.get('/articles/:id', function (req, res) {
+//   db.Article.findOne({ _id: req.params.id })
+//     .populate('note')
+//     .then(function (dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function (err) {
+//       res.json(err);
+//     });
+// });
+
+// app.post('/articles/:id', function (req, res) {
+//   db.Note.create(req.body)
+//     .then(function (dbNote) {
+//       return db.Article.findOneAndUpdate(
+//         { _id: req.params.id },
+//         { note: dbNote._id },
+//         { new: true }
+//       );
+//     })
+//     .then(function (dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function (err) {
+//       res.json(err);
+//     });
+// });
 
 // Start the server
 app.listen(PORT, function () {
