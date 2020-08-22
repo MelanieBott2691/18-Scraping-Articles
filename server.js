d679443f61e490b2745427598398d8a70ae3afe9
MelanieBott2691 <melaniebott@gmail.com>
2020-08-22 00:49:08 -0600

    controller

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-// var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var PORT = process.env.PORT || 3000;
 var app = express();
@@ -9,36 +9,22 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+require('./routes/route');
 
 var MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater';
-var exphbs = require('express-handlebars');
 
 // mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connect('mongodb://localhost/unit18Populater', {
   useNewUrlParser: true
 });
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-require('./routes')(app);
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater');
-// var databaseUri = 'mongodb://localhost/nhlscrape';
-// if (process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI);
-// } else {
-//   mongoose.connect(databaseUri);
-// }
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//   console.log('Connected to Mongoose!');
-// });
-
 // Start the server
 app.listen(PORT, function () {
   console.log('Listening on port ' + PORT + '!');
 });
+module.exports = app;
