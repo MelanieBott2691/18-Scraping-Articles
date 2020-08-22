80e3d7b9c0ea3b708f671e6bacf9eb3fb198273d
MelanieBott2691 <melaniebott@gmail.com>
2020-08-21 18:50:24 -0600

    route to controller

--- server.js
+++ server.js
@@ -29,80 +29,18 @@ mongoose.connect(
 //   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater'
 // )
 // Connect to the Mongo DB
-mongoose.connect('mongodb://localhost/unit18Populater', {
-  useNewUrlParser: true
-});
+// mongoose.connect('mongodb://localhost/unit18Populater', {
+//   useNewUrlParser: true
+// });
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
-
-// Connect to the Mongo DB
-// mongoose.connect(MONGODB_URI);
-
-// Routes
-app.get('/scrape', function (req, res) {
-  axios.get('http://www.kotaku.com/').then(function (response) {
-    var $ = cheerio.load(response.data);
-    $('article h2').each(function (i, element) {
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children('a').text();
-      result.link = $(this).children('a').attr('href');
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function (dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    });
-    res.send('Scrape Complete');
-  });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to Mongoose!');
 });
-
-// Route for getting all Articles from the db
-app.get('/articles', function (req, res) {
-  db.Article.find({})
-    .then(function (dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
-});
-
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get('/articles/:id', function (req, res) {
-  db.Article.findOne({ _id: req.params.id })
-    .populate('note')
-    .then(function (dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
-});
-
-app.post('/articles/:id', function (req, res) {
-  db.Note.create(req.body)
-    .then(function (dbNote) {
-      return db.Article.findOneAndUpdate(
-        { _id: req.params.id },
-        { note: dbNote._id },
-        { new: true }
-      );
-    })
-    .then(function (dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
-});
+var routes = require('./controllers/controller');
+app.use('/', routes);
 
 // Start the server
 app.listen(PORT, function () {
