8f6ddaa32d8ef502d8e36dc902c98f8be4ea8e48
MelanieBott2691 <melaniebott@gmail.com>
2020-08-21 20:45:21 -0600

    moved nav outside partials to views

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
-var express = require('express');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+// var express = require('express');
+// var logger = require('morgan');
+// var mongoose = require('mongoose');
+// var bodyParser = require('body-parser');
+// var app = express();
+
+// var PORT = process.env.PORT || 3000;
+
+// app.use(logger('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(express.static(process.cwd() + '/public'));
+// var exphbs = require('express-handlebars');
+// app.engine(
+//   'handlebars',
+//   exphbs({
+//     defaultLayout: 'main'
+//   })
+// );
+// app.set('view engine', 'handlebars');
+// mongoose.connect(
+//   'mongodb://heroku_wjqkjs6jroot@ds157654.mlab.com:57654/heroku_wjqkjs6j',
+//   { useNewUrlParser: true }
+// );
+// // mongoose.connect(
+// //   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater');
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   console.log('Connected to Mongoose!');
+// });
+// // get routes
+// var routes = require('./controllers/controller');
+// app.use('/', routes);
+
+// // Start the server
+// app.listen(PORT, function () {
+//   console.log('Listening on port ' + PORT + '!');
+// });
+//dependencies
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+
+//initialize Express app
+var express = require('express');
 var app = express();
 
-var PORT = process.env.PORT || 3000;
-
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 app.use(express.static(process.cwd() + '/public'));
+
 var exphbs = require('express-handlebars');
 app.engine(
   'handlebars',
@@ -18,23 +64,24 @@ app.engine(
   })
 );
 app.set('view engine', 'handlebars');
+
+//connecting to MongoDB
 mongoose.connect(
-  'mongodb://heroku_wjqkjs6jroot@ds157654.mlab.com:57654/heroku_wjqkjs6j',
-  { useNewUrlParser: true }
+  'mongodb://heroku_jxkjhg1v:6s68tem51mlionrj2sneb7b53c@ds127988.mlab.com:27988/heroku_jxkjhg1v'
 );
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater');
+
+//mongoose.connect('mongodb://localhost/scraped_news');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Connected to Mongoose!');
 });
-// get routes
+
 var routes = require('./controllers/controller');
 app.use('/', routes);
 
-// Start the server
-app.listen(PORT, function () {
-  console.log('Listening on port ' + PORT + '!');
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Listening on PORT ' + port);
 });
