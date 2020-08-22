51ca582236aa2e1e6356b30ecd4bcebb615b58fc
MelanieBott2691 <melaniebott@gmail.com>
2020-08-21 19:13:45 -0600

    delete deploy tools and cleaning up

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ var express = require('express');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-// var axios = require('axios');
-// var cheerio = require('cheerio');
-
-var db = require('./models');
-var PORT = process.env.PORT || 3000;
 var app = express();
+
+var PORT = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+
 app.use(express.static(process.cwd() + '/public'));
 var exphbs = require('express-handlebars');
 app.engine(
@@ -23,29 +19,19 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 mongoose.connect(
-  'mongodb://heroku_wjqkjs6j.user1:root@ds157654.mlab.com:57654/heroku_wjqkjs6j'
+  'mongodb://heroku_wjqkjs6jroot@ds157654.mlab.com:57654/heroku_wjqkjs6j'
 );
 // mongoose.connect(
 //   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater'
-// )
-// Connect to the Mongo DB
-// mongoose.connect('mongodb://localhost/unit18Populater', {
-//   useNewUrlParser: true
-// });
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Connected to Mongoose!');
 });
+// get routes
 var routes = require('./controllers/controller');
 app.use('/', routes);
-
-// MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
-
-// Connect to the Mongo DB
-// mongoose.connect(MONGODB_URI);
 
 // Start the server
 app.listen(PORT, function () {
