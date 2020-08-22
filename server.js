47be4ff717c03848016f2c73bf0a4ea646240d89
MelanieBott2691 <melaniebott@gmail.com>
2020-08-21 21:47:01 -0600

    changed controller to app

--- server.js
+++ server.js
@@ -1,87 +1,45 @@
-// var express = require('express');
-// var logger = require('morgan');
-// var mongoose = require('mongoose');
-// var bodyParser = require('body-parser');
-// var app = express();
-
-// var PORT = process.env.PORT || 3000;
-
-// app.use(logger('dev'));
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(express.static(process.cwd() + '/public'));
-// var exphbs = require('express-handlebars');
-// app.engine(
-//   'handlebars',
-//   exphbs({
-//     defaultLayout: 'main'
-//   })
-// );
-// app.set('view engine', 'handlebars');
-// mongoose.connect(
-//   'mongodb://heroku_wjqkjs6jroot@ds157654.mlab.com:57654/heroku_wjqkjs6j',
-//   { useNewUrlParser: true }
-// );
-// // mongoose.connect(
-// //   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater');
-
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//   console.log('Connected to Mongoose!');
-// });
-// // get routes
-// var routes = require('./controllers/controller');
-// app.use('/', routes);
-
-// // Start the server
-// app.listen(PORT, function () {
-//   console.log('Listening on port ' + PORT + '!');
-// });
-//dependencies
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var logger = require('morgan');
-
-//initialize Express app
 var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 
+var PORT = process.env.PORT || 3000;
+
 app.use(logger('dev'));
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(process.cwd() + '/public'));
-
 var exphbs = require('express-handlebars');
 app.engine(
   'handlebars',
-  exphbs({
-    defaultLayout: 'main'
-  })
+  exphbs({ defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts' })
 );
 app.set('view engine', 'handlebars');
 
-//connecting to MongoDB
 mongoose.connect(
-  'mongodb://heroku_jxkjhg1v:6s68tem51mlionrj2sneb7b53c@ds127988.mlab.com:27988/heroku_jxkjhg1v'
+  'mongodb://heroku_wjqkjs6jroot@ds157654.mlab.com:57654/heroku_wjqkjs6j',
+  { useNewUrlParser: true }
 );
-
-//mongoose.connect('mongodb://localhost/scraped_news');
-
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater');
+var databaseUri = 'mongodb://localhost/nhlscrape';
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Connected to Mongoose!');
 });
-
+// get routes
 var routes = require('./controllers/controller');
 app.use('/', routes);
 
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Listening on PORT ' + port);
+// Start the server
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT + '!');
 });
+dependencies;
