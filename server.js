5e1d39d9e604090d020c3eb1e79cc24c31b165f3
MelanieBott2691 <melaniebott@gmail.com>
2020-08-23 01:17:23 -0600

    added result title, summary to results

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var path = require('path');
-
 // var bodyParser = require('body-parser');
 // var logger = require('morgan');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var cheerio = require('cheerio');
+var axios = require('axios');
+var db = require('./models');
 
 var PORT = process.env.PORT || 3000;
 var app = express();
@@ -13,13 +14,13 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + './public/index.html'));
-});
-// var exphbs = require('express-handlebars');
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname + './public/index.html'));
+// });
+var exphbs = require('express-handlebars');
 
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // var db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'connection error:'));
@@ -28,10 +29,9 @@ app.get('/', function (req, res) {
 // });
 // // require('./routes/route');
 
-// var MONGODB_URI =
-//   process.env.MONGODB_URI || 'mongodb://localhost/unit18Populater';
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoscraper';
 // mongodb+srv://user1:<password>@mongoscraper.cfphn.mongodb.net/<dbname>?retryWrites=true&w=majority
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // // mongoose.connect('mongodb://localhost/unit18Populater', {
 // //   useUnifiedTopology: true
 // // });
@@ -39,103 +39,61 @@ app.get('/', function (req, res) {
 // var routes = require('./controller/controller');
 // app.use('/', routes);
 
-// Start the server
-app.listen(PORT, function () {
-  console.log('Listening on port ' + PORT + '!');
+// // A GET route for scraping the echoJS website
+app.get('/scrape', function (req, res) {
+  //   // First, we grab the body of the html with axios
+  axios.get('https://kotaku.com/').then(function (response) {
+    //     // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    //     // Now, we grab every h2 within an article tag, and do the following:
+    $('article').each(function (i, element) {
+      //       // Save an empty result object
+      var result = {};
+
+      //       // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children().children().text();
+      result.link =
+        'https://www.kotaku.com' +
+        $(this).children('a').children('a').attr('href');
+      result.summary = $(this).children('p + p').text();
+      result.image = $(this).children('div').children('img').attr('src');
+
+      var titleCut = result.title.split('Continue', 1);
+      console.log(titleCut);
+      result.title = titleCut[0];
+      result.summary = result.summary.replace(/ \ /g, '');
+      result.title = result.title.replace(/ \ /g, '');
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          //           // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          //           // If an error occurred, log it
+          console.log(err);
+        });
+    });
+    res.redirect('/');
+  });
 });
 
-// //  Test //
-// var express = require('express');
-// var logger = require('morgan');
-// var mongoose = require('mongoose');
-
-// // Our scraping tools
-// // Axios is a promised-based http library, similar to jQuery's Ajax method
-// // It works on the client and on the server
-// var axios = require('axios');
-// var cheerio = require('cheerio');
-
-// // Require all models
-// var db = require('./models');
-
-// var PORT = process.env.PORT || 3000;
-// var MONGODB_URI =
-//   process.env.MONGODB_URI ||
-//   'mongodb+srv://user1:<abc123>@mongoscraper.cfphn.mongodb.net/<dbname>?retryWrites=true&w=majority';
-
-// // Initialize Express
-// var app = express();
-
-// // Configure middleware
-
-// // // Use morgan logger for logging requests
-// // app.use(logger('dev'));
-// // // Parse request body as JSON
-// // app.use(express.urlencoded({ extended: true }));
-// // app.use(express.json());
-// // // Make public a static folder
-// // app.use(express.static('public'));
-
-// app.use(logger('dev'));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static('public'));
-
-// // app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// // app.set('view engine', 'handlebars');
-// // Connect to the Mongo DB
-// mongoose.connect('mongodb://localhost/mongoscraper', {
-//   useNewUrlParser: true
-// });
-
-// // Routes
-
-// // A GET route for scraping the echoJS website
-// app.get('/scrape', function (req, res) {
-//   // First, we grab the body of the html with axios
-//   axios.get('https://kotaku.com/').then(function (response) {
-//     // Then, we load that into cheerio and save it to $ for a shorthand selector
-//     var $ = cheerio.load(response.data);
-
-//     // Now, we grab every h2 within an article tag, and do the following:
-//     $('article').each(function (i, element) {
-//       // Save an empty result object
-//       var result = {};
-
-//       // Add the text and href of every link, and save them as properties of the result object
-//       result.title = $(this).children('a').text();
-//       result.link = $(this).children('a').attr('href');
-
-//       // Create a new Article using the `result` object built from scraping
-//       db.Article.create(result)
-//         .then(function (dbArticle) {
-//           // View the added result in the console
-//           console.log(dbArticle);
-//         })
-//         .catch(function (err) {
-//           // If an error occurred, log it
-//           console.log(err);
-//         });
-//     });
-
-//     // Send a message to the client
-//     res.send('Scrape Complete');
-//   });
-// });
-
 // // Route for getting all Articles from the db
-// app.get('/articles', function (req, res) {
-//   // Grab every document in the Articles collection
-//   db.Article.find({})
-//     .then(function (dbArticle) {
-//       // If we were able to successfully find Articles, send them back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function (err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+app.get('/', function (err, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function (dbArticle) {
+      //       // If we were able to successfully find Articles, send them back to the client
+      res.render('index', {
+        articles: dbArticle
+      });
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 // // Route for grabbing a specific Article by id, populate it with it's note
 // app.get('/articles/:id', function (req, res) {
