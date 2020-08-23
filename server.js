49013f9918e70c1dcfe86da07d52a138447496f0
MelanieBott2691 <melaniebott@gmail.com>
2020-08-23 16:38:25 -0600

    reverted to original, not sure why it isn't working on heroku but it is reverted there too

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ var MONGODB_URI =
 app.get('/scrape', function (req, res) {
   axios.get('http://www.kotaku.com/').then(function (response) {
     var $ = cheerio.load(response.data);
-    $('article h2').each(function (i, element) {
+    $('article').each(function (i, element) {
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children('a').text();
-      result.link = $(this).children('a').attr('href');
-
+      result.title = $(this).children('h4').last().text();
+      result.link = $(this).children('a').last().attr('href');
+      result.img = $(this).children('div').last().attr('href');
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function (dbArticle) {
